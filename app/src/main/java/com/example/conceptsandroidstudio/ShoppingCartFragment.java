92e4b6797e73f4849d53b07e1238a0283266c204
rogelocard <rsca@misena.edu.co>
2024-05-24 20:01:22 -0500

    Added geolocation when adding a shipping address

--- app/src/main/java/com/example/conceptsandroidstudio/ShoppingCartFragment.java
+++ app/src/main/java/com/example/conceptsandroidstudio/ShoppingCartFragment.java
@@ -1,12 +1,51 @@
 package com.example.conceptsandroidstudio;
 
+import android.content.DialogInterface;
+import android.location.Address;
 import android.os.Bundle;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.Fragment;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.Geocoder;
+import android.text.InputType;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.Fragment;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.Priority;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -14,6 +53,14 @@ import android.view.ViewGroup;
  * create an instance of this fragment.
  */
 public class ShoppingCartFragment extends Fragment {
+
+//    private static final int LOCATION_PERMISSION_REQUEST_CODE = 1;
+    private FusedLocationProviderClient fusedLocationProviderClient;
+    private LocationCallback locationCallback;
+    private FirebaseFirestore db;
+    private FirebaseAuth mAuth;
+    private LinearLayout addressContainer;
+    private List<String> addressList = new ArrayList<>();
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -53,12 +100,174 @@ public class ShoppingCartFragment extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+
+        mAuth = FirebaseAuth.getInstance();
+        db = FirebaseFirestore.getInstance();
+        // Initialize FusedLocationProviderClient
+         fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(requireActivity());
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_shopping_cart, container, false);
+        //return inflater.inflate(R.layout.fragment_shopping_cart, container, false);
+        View view = inflater.inflate(R.layout.fragment_shopping_cart, container, false);
+
+        TextView addAddressTextView = view.findViewById(R.id.add_address_text_view);
+        addressContainer = view.findViewById(R.id.address_container);
+
+        addAddressTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                showAddressDialog();
+                if (ContextCompat.checkSelfPermission(requireActivity(), Manifest.permission.ACCESS_FINE_LOCATION)
+                        != PackageManager.PERMISSION_GRANTED) {
+                    requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+                } else {
+                    getCurrentLocation();
+                }
+            }
+        });
+
+        loadAddresses();
+
+        return view;
     }
+
+    /**
+     * Function to check the user's answer to the permission request.
+     * @param requestCode The request code passed in {@link #requestPermissions(String[], int)}.
+     * @param permissions The requested permissions. Never null.
+     * @param grantResults The grant results for the corresponding permissions
+     *     which is either {@link android.content.pm.PackageManager#PERMISSION_GRANTED}
+     *     or {@link android.content.pm.PackageManager#PERMISSION_DENIED}. Never null.
+     *
+     */
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == 1) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                getCurrentLocation();
+            } else {
+                Toast.makeText(requireActivity(), "Permiso de ubicación denegado", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    private void getCurrentLocation() {
+        if (ContextCompat.checkSelfPermission(requireActivity(), Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+            return;
+        }
+        fusedLocationProviderClient.getLastLocation().addOnCompleteListener(new OnCompleteListener<Location>() {
+            @Override
+            public void onComplete(@NonNull Task<Location> task) {
+                if (task.isSuccessful() && task.getResult() != null) {
+                    Location location = task.getResult();
+                    getAddressFromLocation(location);
+                } else {
+                    requestNewLocationData();
+                }
+            }
+        });
+    }
+
+    private void requestNewLocationData() {
+        LocationRequest locationRequest = new LocationRequest.Builder(Priority.PRIORITY_HIGH_ACCURACY, 10000)
+                .setWaitForAccurateLocation(false)
+                .setMinUpdateIntervalMillis(5000)
+                .setMaxUpdateDelayMillis(10000)
+                .build();
+
+        locationCallback = new LocationCallback() {
+            @Override
+            public void onLocationResult(LocationResult locationResult) {
+                if (locationResult == null) {
+                    Toast.makeText(requireActivity(), "No se pudo obtener la ubicación", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                Location location = locationResult.getLastLocation();
+                getAddressFromLocation(location);
+                fusedLocationProviderClient.removeLocationUpdates(locationCallback);
+            }
+        };
+
+        if (ContextCompat.checkSelfPermission(requireActivity(), Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            fusedLocationProviderClient.requestLocationUpdates(locationRequest, locationCallback, null);
+        }
+    }
+
+    private void getAddressFromLocation(Location location) {
+        Geocoder geocoder = new Geocoder(requireActivity(), Locale.getDefault());
+        try {
+            List<Address> addresses = geocoder.getFromLocation(location.getLatitude(), location.getLongitude(), 1);
+            if (addresses != null && !addresses.isEmpty()) {
+                Address address = addresses.get(0);
+                String addressText = address.getAddressLine(0);
+                saveAddress(addressText);
+            } else {
+                Toast.makeText(requireActivity(), "No se pudo obtener la dirección", Toast.LENGTH_SHORT).show();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            Toast.makeText(requireActivity(), "Error obteniendo la dirección", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void saveAddress(String address) {
+        if (addressList.size() >= 3) {
+            // Remove the first address and show a message
+            addressList.remove(0);
+            Toast.makeText(requireActivity(), "Se ha eliminado la primera dirección para añadir la nueva.", Toast.LENGTH_SHORT).show();
+        }
+        addressList.add(address);
+        CollectionReference addressesRef = db.collection("user").document(mAuth.getCurrentUser().getUid()).collection("addresses");
+
+//        Map<String, Object> addresses = new HashMap<>();
+//        addresses.put("addresses", addressList);
+        Map<String, Object> addressMap = new HashMap<>();
+        addressMap.put("address", address);
+
+        addressesRef.add(addressMap).addOnCompleteListener(new OnCompleteListener<DocumentReference>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentReference> task) {
+                if (task.isSuccessful()) {
+                    displayAddresses();
+                }
+            }
+        });
+
+    }
+
+    private void loadAddresses() {
+        CollectionReference addressesRef = db.collection("user").document(mAuth.getCurrentUser().getUid()).collection("addresses");
+
+        addressesRef.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                if (task.isSuccessful() && !task.getResult().isEmpty()) {
+                    addressList.clear();
+                    for (DocumentSnapshot document : task.getResult()) {
+                        addressList.add(document.getString("address"));
+                    }
+                    displayAddresses();
+                }
+            }
+        });
+    }
+
+    private void displayAddresses() {
+        addressContainer.removeAllViews();
+        for (String address : addressList) {
+            TextView addressTextView = new TextView(requireActivity());
+            addressTextView.setText(address);
+            addressTextView.setPadding(8, 8, 8, 8);
+            addressTextView.setTextSize(16);
+            addressContainer.addView(addressTextView);
+        }
+    }
+
 }
